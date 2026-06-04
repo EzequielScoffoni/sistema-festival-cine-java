c9cfc4f282b696dcfb59d8442cdf71c8e57d20a7
Ezequiel Scoffoni <eze.esco@gmail.com>
2026-06-03 22:53:28 -0300

    add consola and main up ok

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,10 +4,11 @@ import pesistencia.FuncionCSV;
 import pesistencia.PeliculaCSV;
 
 import java.util.HashSet;
+import ui.Consola;
 
 public class Main {
     public static void main(String[] args) {
-        // Prueba de clases, metodos y persistencia (peliculas.csv)
+        /* //Prueba de clases, metodos y persistencia (peliculas.csv)
         Director director1 = new Director("Christopher", "Nolan", 55, "30/07/1970", "masculino");
         Pelicula pelicula1 = new Pelicula("Interestelar", "Ficcion", 180, director1);
         PeliculaCSV peliculaCSV1 = new PeliculaCSV();
@@ -22,7 +23,9 @@ public class Main {
 
         FuncionCSV funcionCSV1 = new FuncionCSV();
         funcionCSV1.guardarFuncion(funcion1);
-        funcionCSV1.leerFuncion();
+        funcionCSV1.leerFuncion(); */
+        Consola consola = new Consola();
+        consola.iniciar();
 
     }
 }
--- src/ui/Consola.java
+++ src/ui/Consola.java
@@ -1,11 +1,13 @@
 package ui;
 
 import modelo.*;
+import pesistencia.EspectadorCSV;
 import pesistencia.FuncionCSV;
 import pesistencia.PeliculaCSV;
 
 import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Consola {
     public void iniciar () {
@@ -83,7 +85,7 @@ public class Consola {
                     System.out.println("Las ediciones se visualizan desde las funciones registradas.");
                     break;
                 case 0:
-                    System.out.println("Saliendo...");
+                    System.out.println("Volviendo...");
             }
         }
     }
@@ -291,13 +293,64 @@ public class Consola {
             scanner.nextLine();
             switch (opcion){
                 case 1:
+                    System.out.println("Ingresa el nombre del espectador: ");
+                    String nombreEspectador = scanner.nextLine();
+                    System.out.println("Ingresa el apellido del espectador: ");
+                    String apellidoEspectador = scanner.nextLine();
+                    System.out.println("Ingresa la edad del espectador: ");
+                    int edadEspectador = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("Ingresar la fecha de nacimiento del espectador: ");
+                    String fechaNacimientoEspectador = scanner.nextLine();
+                    System.out.println("Ingresar el sexo del espectador: ");
+                    String sexoEspectador = scanner.nextLine();
+                    System.out.println("Ingrese el dni del espectador: ");
+                    int dniEspectador = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("Ingrese el email del espectador: ");
+                    String emailEspectador = scanner.nextLine();
+                    Espectador espectador = new Espectador(nombreEspectador, apellidoEspectador, edadEspectador, fechaNacimientoEspectador, sexoEspectador, dniEspectador, emailEspectador);
+                    EspectadorCSV espectadorCSV = new EspectadorCSV();
+                    espectadorCSV.guardarEspectador(espectador);
+                    System.out.println("Espectador registrado correctamente");
                     break;
                 case 2:
+                    System.out.println("Ingresa el nombre del espectador: ");
+                    String nombreEspectadorV = scanner.nextLine();
+                    System.out.println("Ingresa el apellido del espectador: ");
+                    String apellidoEspectadorV = scanner.nextLine();
+                    System.out.println("Ingrese el dni del espectador: ");
+                    int dniEspectadorV = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("Ingrese el email del espectador: ");
+                    String emailEspectadorV = scanner.nextLine();
+                    System.out.println("Ingrese el nro de la butaca que desea: ");
+                    int nroButaca = scanner.nextInt();
+                    System.out.println("Ingrese el nro de la fila que desea: ");
+                    int nroFila = scanner.nextInt();
+                    scanner.nextLine();
+                    Espectador espectadorV = new Espectador(nombreEspectadorV, apellidoEspectadorV, 27, "30/06/1998", "masculino", dniEspectadorV, emailEspectadorV);
+                    Festival festivalV = new Festival("Nuevo festival de cine");
+                    Director director = new Director("Quentin", "Tarantino", 63, "27/03/1963", "masculino");
+                    Pelicula pelicula = new Pelicula("Once Upon a Time in Hollywood", "Ficcion", 200, director);
+                    Edicion edicion = new Edicion(1, "Buenos Aires", "30/06/2026", festivalV);
+                    Set<Butaca> butacas = new HashSet<>();
+                    Butaca butaca = new Butaca(nroButaca, nroFila, true);
+                    butacas.add(butaca);
+                    Sala sala = new Sala(1, butacas);
+                    Funcion funcion = new Funcion("30/06/2026", "20:00", pelicula, edicion, sala, new HashSet<>());
+                    Entrada entrada = new Entrada(espectadorV, funcion, butaca);
+                    if (entrada.generarEntrada()){
+                        System.out.println("Entrada vendida correctamente.");
+                    } else {
+                        System.out.println("No se pudo vender la entrada.");
+                    }
                     break;
                 case 3:
+                    System.out.println("Las entradas se visualizan al momento de la venta.");
                     break;
                 case 0:
-                    System.out.println("Saliendo...");
+                    System.out.println("Volviendo...");
             }
         }
     }
@@ -315,13 +368,65 @@ public class Consola {
             scanner.nextLine();
             switch (opcion){
                 case 1:
+                    System.out.println("Ingrese el nombre del jurado: ");
+                    String nombreJurado = scanner.nextLine();
+                    System.out.println("Ingrese el apellido del jurado: ");
+                    String apellidoJurado = scanner.nextLine();
+                    System.out.println("Ingrese el titulo de la pelicula: ");
+                    String tituloPelicula = scanner.nextLine();
+                    System.out.println("Ingrese el puntaje: ");
+                    int puntaje = scanner.nextInt();
+                    scanner.nextLine();
+                    Jurado jurado = new Jurado(nombreJurado, apellidoJurado, 40, "01/06/1986", "masculino");
+                    Director director = new Director("Christopher", "Nolan", 64, "23/05/1962", "masculino");
+                    Pelicula pelicula = new Pelicula(tituloPelicula, "Ficcion", 200, director);
+                    Evaluacion evaluacion = new Evaluacion(puntaje, pelicula, jurado);
+                    if (jurado.hacerEvaluacion(evaluacion)){
+                        pelicula.agregarEvaluacion(evaluacion);
+                        System.out.println("Evaluacion registrada correctamente");
+                    } else {
+                        System.out.println("No se pudo registrar la evaluacion");
+                    }
                     break;
                 case 2:
+                    Director directorP = new Director("Christopher", "Nolan", 64, "24/05/1962", "masculino");
+                    Pelicula peliculaP = new Pelicula("Interestelar", "Ficcion", 240, directorP);
+                    Jurado jurado1 = new Jurado("Ezequiel", "Scoffoni", 27, "30/06/1998", "masculino");
+                    Jurado jurado2 = new Jurado("Juan", "Lopez", 27, "01/08/1998", "masculino");
+                    Evaluacion evaluacion1 = new Evaluacion(7, peliculaP, jurado1);
+                    Evaluacion evaluacion2 = new Evaluacion(10, peliculaP, jurado2);
+                    peliculaP.agregarEvaluacion(evaluacion1);
+                    peliculaP.agregarEvaluacion(evaluacion2);
+                    double promedio = peliculaP.calcularPromedioEvaluaciones();
+                    System.out.println("Promedio de las evaluaciones: " + promedio);
                     break;
                 case 3:
+                    Director directorA = new Director("Christopher", "Nolan", 64, "24/05/1962", "masculino");
+                    Director directorB = new Director("Quentin", "Tarantino", 63, "27/03/1963", "masculino");
+                    Pelicula peliculaA = new Pelicula("Interestelar", "Ficcion", 240, directorA);
+                    Pelicula peliculaB = new Pelicula("Pulp Fuction", "Drama", 180, directorB);
+                    Jurado juradoA = new Jurado("Juan", "Lopez", 27, "01/08/1998", "masculino");
+                    Jurado juradoB = new Jurado("Ezequiel","Scoffoni", 27, "30/06/1998", "masculino");
+                    Evaluacion evaluacionA = new Evaluacion(7, peliculaA, juradoA);
+                    Evaluacion evaluacionB = new Evaluacion(10, peliculaA, juradoB);
+                    Evaluacion evaluacionC = new Evaluacion(8, peliculaB, juradoA);
+                    Evaluacion evaluacionD = new Evaluacion(6, peliculaB, juradoB);
+                    peliculaA.agregarEvaluacion(evaluacionA);
+                    peliculaA.agregarEvaluacion(evaluacionB);
+                    peliculaB.agregarEvaluacion(evaluacionC);
+                    peliculaB.agregarEvaluacion(evaluacionD);
+                    double promedioA = peliculaA.calcularPromedioEvaluaciones();
+                    double promedioB = peliculaB.calcularPromedioEvaluaciones();
+                    if (promedioA > promedioB) {
+                        System.out.println("La pelicula ganadora es: Interestelar");
+                    } else if (promedioB > promedioA) {
+                        System.out.println("La pelicula ganadora es: Pulp Fiction");
+                    } else {
+                        System.out.println("Hay un empate entre las peliculas.");
+                    }
                     break;
                 case 0:
-                    System.out.println("Saliendo...");
+                    System.out.println("Volviendo...");
             }
         }
     }
